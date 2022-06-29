e05fd12ea6dc7fb2697b77414fe833f78f1bbe02
Patrick Luan <you@your-domain.com>
2022-06-28 22:50:00 -0300

    Add new components on App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <template>
-  <header>
+  <header class="header">
     <h1>ALT-Calendar</h1>
   </header>
 
-  <main>
+  <main style="display:flex;column-gap:10px;">
+    <div>
+      <CalendarWidget :startAt.sync="startDate" :businessDays.sync="businessDays" :holidays.sync="holidays">
+      </CalendarWidget>
+      <SettingsWidget @apply="applySettings"></SettingsWidget>
+    </div>
+    <DaysWidget v-if="false"></DaysWidget>
   </main>
 </template>
 
 <script setup>
+import CalendarWidget from '@/components/CalendarWidget.vue'
+import DaysWidget from '@/components/DaysWidget.vue'
+import SettingsWidget from '@/components/SettingsWidget.vue'
+import { ref } from "vue"
+
+const startDate = ref('')
+const businessDays = ref(false)
+const holidays = ref(false)
+
+const applySettings = (newSettings) => {
+  startDate.value = newSettings.startDate
+  businessDays.value = newSettings.businessDays
+  holidays.value = newSettings.holidays
+}
 </script>
 
 <style>
