595aa2f80583ddb1c812c51780c51f176b8fab92
Patrick Luan <you@your-domain.com>
2022-06-28 22:49:16 -0300

    Create DaysWidget component

---
+++ src/components/DaysWidget.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="card list">
+    <div class="list-item" v-for="i in 5">
+      <div>
+        <div class="day-title">{{ i * 3 + Math.floor(Math.random() * 3) }}</div>
+        <div class="day-subtitle">Quarta-feita</div>
+      </div>
+      <div>
+
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+</script>
+
+<style>
+.day-title {
+  font-size: 38px;
+  color: #555;
+}
+
+.day-subtitle {
+  font-size: 12px;
+  color: #999;
+}
+</style>
